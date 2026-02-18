1922dfb946213092c7bcbbe5a6e55daacc260deb
Deepak Repo <dpk.ewit@gmail.com>
2026-02-18 23:45:55 +0530

    Add success message to helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,6 @@
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("Hello, Deepak in Git hub!");
+        System.out.println("triger, it's triger sucssfull once change commit done!");
     }
 }
