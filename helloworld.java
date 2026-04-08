04d6b96daa67adf83379647ec7abaafcbae7d6b9
Deepak Repo <dpk.ewit@gmail.com>
2026-04-08 11:52:29 +0530

    Update greeting and pipeline success message

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello, Deepak kumar Sinha in Git hub!");
-        System.out.println("pipeline, it's pipeline sucssfull once change done 2nd April!");
+        System.out.println("Hello, Ramesh kumar Sinha in Git hub!");
+        System.out.println("pipeline, it's pipeline sucssfull once change done 8th April!");
     }
 }
